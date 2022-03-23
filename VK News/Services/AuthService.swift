f052f785e8c4e8898015361cabb01a965b032571
ValentinaLuchinovich <valinkontakt11@bk.ru>
2022-03-23 12:25:59 +0300

    Initial Commit

---
+++ VK News/Services/AuthService.swift
@@ -0,0 +1,43 @@
+//
+//  AuthService.swift
+//  VK News
+//
+//  Created by Валентина Лучинович on 22.03.2022.
+//
+
+import Foundation
+import VKSdkFramework
+
+final class AuthService: NSObject, VKSdkDelegate, VKSdkUIDelegate {
+    
+    private let appId = "8112106"
+    private let vkSdk: VKSdk
+    
+    override init() {
+        vkSdk = VKSdk.initialize(withAppId: appId)
+        super.init()
+        print("VKSdk.initialize")
+        vkSdk.register(self)
+        vkSdk.uiDelegate = self
+    }
+    
+    // MARK: VKSdkDelegate
+    func vkSdkAccessAuthorizationFinished(with result: VKAuthorizationResult!) {
+        print(#function)
+    }
+    
+    func vkSdkUserAuthorizationFailed() {
+        print(#function)
+    }
+    
+    // MARK: VKSdkUIDelegate
+    
+    func vkSdkShouldPresent(_ controller: UIViewController!) {
+        print(#function)
+    }
+    
+    func vkSdkNeedCaptchaEnter(_ captchaError: VKError!) {
+        print(#function)
+    }
+    
+}
---
+++ VK News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK News
+//
+//  Created by Валентина Лучинович on 22.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
