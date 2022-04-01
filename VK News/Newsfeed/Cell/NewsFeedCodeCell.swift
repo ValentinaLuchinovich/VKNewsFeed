35fd0281f4a269563982fa2b2a337e8476632b4e
ValentinaLuchinovich <valinkontakt11@bk.ru>
2022-04-01 20:45:48 +0300

    Реализация кликабельны ссылок.

--- VK News/Newsfeed/Cell/NewsFeedCodeCell.swift
+++ VK News/Newsfeed/Cell/NewsFeedCodeCell.swift
@@ -33,13 +33,28 @@ final class NewsFeedCodeCell: UITableViewCell {
         return view
     }()
     
-    let postLabel: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.font = Constants.postLabelFont
-        label.textColor = #colorLiteral(red: 0.2276484668, green: 0.2322214842, blue: 0.236567229, alpha: 1)
-        
-        return label
+//    let postLabel: UILabel = {
+//        let label = UILabel()
+//        label.numberOfLines = 0
+//        label.font = Constants.postLabelFont
+//        label.textColor = #colorLiteral(red: 0.2276484668, green: 0.2322214842, blue: 0.236567229, alpha: 1)
+//
+//        return label
+//    }()
+    
+    let postLabel: UITextView = {
+        let textView = UITextView()
+        textView.font = Constants.postLabelFont
+        textView.isScrollEnabled = false
+        textView.isSelectable = true
+        textView.isUserInteractionEnabled = true
+        textView.isEditable = false
+        textView.backgroundColor = .clear
+        textView.textColor = #colorLiteral(red: 0.2276484668, green: 0.2322214842, blue: 0.236567229, alpha: 1)
+        let padding = textView.textContainer.lineFragmentPadding
+        textView.textContainerInset = UIEdgeInsets.init(top: 0, left: -padding , bottom: 0, right: -padding)
+        textView.dataDetectorTypes = UIDataDetectorTypes.all
+        return textView
     }()
     
     let moreTextButton: UIButton = {
